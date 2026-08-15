707d64a133847244d618637e707e95f784d8a676
haroon239 <haroon116butt@gmail.com>
2026-08-15 20:46:08 +0500

    add new navigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -2,5 +2,6 @@ export const primaryNavigation = [
   { label: "Home", href: "/#home", hasMenu: false },
   { label: "About Us", href: "/#about", hasMenu: false },
   { label: "Services", href: "/#services", hasMenu: false },
+  { label: "Locations", href: "/#locations", hasMenu: false },
   { label: "Contact", href: "/#contact", hasMenu: false },
 ] as const;
